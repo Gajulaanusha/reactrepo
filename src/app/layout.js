886c409c8985e699102ecae79bbe70516ae01711
Anusha <gajulaanusha2005@gmail.com>
2024-08-17 09:33:07 +0530

    latest

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Header } from "@/Header"
 import {Footer} from "@/Footer"
 import {Menu} from "@/Menu"
+
 import "./globals.css"
 export default function RootLayout({ children }) {
   return (
